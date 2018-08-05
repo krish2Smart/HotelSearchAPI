dca8539bf0bac643aa7fa64d9dac1ce3c5e8eba2
Ragul Krishnan A <krishragul143@gmail.com>
2018-08-06 00:39:23 +0530

    version 1.0

---
+++ HoteleSearchApi/Controllers/HotelSearchController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HoteleSearchApi.Controllers
+{
+    public class HotelSearchController : Controller
+    {
+        private string _city;
+        private string _latitude, _longitude;
+        private static List<Hotels> 
+
+    }
+}
---
+++ HoteleSearchApi/Models/Hotels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoteleSearchApi.Models
+{
+    public class Hotels
+    {
+        public string HotelName { get; set; }
+
+        public string Address { get; set; } 
+    }
+}
